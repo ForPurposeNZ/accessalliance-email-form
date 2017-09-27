ef870af00a6c846d784381f045b2f834fe1bd6fb
John Miller-Williams <john@miller-williams.name>
2017-09-28 11:33:45 +1300

    Added test event

--- update-access-email.js
+++ update-access-email.js
@@ -1,15 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("Hello Test!");
+var ELECTORATE_DROPDOWN_ID = "#id123-control31500616";
+var EMAIL_ELEMENT_ID = "#id123-control31500105";
+var HEADER_MARKER_SPLITTER = "###";
 
-  var url = $("input[name=tmp_form_host]").val();
-  console.log("url", url);
+function getParameterByName(name, url) {
+  console.log("made it to getParameterByName");
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-  var electorate = getParameterByName("electorate", url);
-  console.log("electorate", electorate);
-  $("#id123-control32357384")
-    .val(electorate)
-    .change();
-
+function replaceParameterContent() {
   var insertContent = "\n";
 
   var kids = getParameterByName("kids", url);
@@ -34,7 +38,9 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("total", total);
   if (total)
     insertContent += "Total people with access sensitivity: " + total + "\n";
+}
 
+function replaceLetterContent() {
   var emailBody = $("#id123-control32357387");
   var prevEmailBody = emailBody.val();
   console.log("prevEmailBody", prevEmailBody);
@@ -42,14 +48,25 @@ document.addEventListener("DOMContentLoaded", function() {
   var updatedEmailBody = prevEmailBody.replace("@@@@", insertContent);
   console.log("updatedEmailBody", updatedEmailBody);
   emailBody.val(updatedEmailBody);
+}
 
-  function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return "";
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
+function dropdownChanged() {
+  Console.log("Changed!");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Hello Test!");
+
+  var url = $("input[name=tmp_form_host]").val();
+  console.log("url", url);
+
+  $(ELECTORATE_DROPDOWN_ID).on("changed", dropdownChanged);
+  //replaceLetterContent();
+
+  // set initial electorate from parameter
+  var electorate = getParameterByName("electorate", url);
+  console.log("electorate", electorate);
+  $(ELECTORATE_DROPDOWN_ID)
+    .val(electorate)
+    .change();
 });
